f2aa96caea941b88895289253cc11cce389849d4
Jodi Pranata <jodranata@gmail.com>
2020-02-05 16:36:59 +0700

    Updates

---
+++ precache-manifest.589770d2fc1ba99dc3db3c6dfb3bb5cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f5eca4ef6a8331ee0081aae3f2a4271",
+    "url": "/FCC-quotegenerator/index.html"
+  },
+  {
+    "revision": "e17b05ea46711225e08c",
+    "url": "/FCC-quotegenerator/static/css/main.a91af053.chunk.css"
+  },
+  {
+    "revision": "2c9a23fee59c885b9bab",
+    "url": "/FCC-quotegenerator/static/js/2.6305feb7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/FCC-quotegenerator/static/js/2.6305feb7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e17b05ea46711225e08c",
+    "url": "/FCC-quotegenerator/static/js/main.0c9c440f.chunk.js"
+  },
+  {
+    "revision": "941eeeb2e1e0b6aa344d",
+    "url": "/FCC-quotegenerator/static/js/runtime-main.7ac543a3.js"
+  }
+]);
